8a092979f78f642be01011919c45627d36881ac7
madeline allison restin <144450180+maddycode15@users.noreply.github.com>
2023-11-06 09:38:04 -0400

    Initial commit

---
+++ Double Nested loops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Nested_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Double nested loops");
+            Console.WriteLine("a loop within a loop (like inception)");
+            Console.WriteLine();
+
+            for (int i = 1; i <=9; i++) ; //
+
+            {
+                for (int i = 1; i <= 9; i++) ; //
+
+                {
+                    Console.WriteLine(1);
+                }
+
+                Console.WriteLine(1);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("press any key to exit");
+
+            Console.ReadKey();
+        }
+    }
+}
